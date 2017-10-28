d501f47c3ec3ddde676ec287ead117271922abff
RohitKotalwar <rohitkotalwar@Rohits-MacBook-Pro.local>
2017-10-27 23:33:21 -0700

    First commit

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/Doc/Software_Fulltime.pdf', function(request, response) {
+  response.render('Doc/Software_Fulltime');
+});
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
